ac5ce2492d77ae782644b326255fac3c7cf45cf2
Asid <asidpadop@gmail.com>
2024-01-03 10:08:08 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
 import Home from './screens/Home';
 import About from './screens/About';
 import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path='/' element={<Home />}>̦</Route>
         <Route path='/about' element={<About/>}>̦</Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 export default App;
